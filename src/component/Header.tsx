0703a9e471bfb170572f877f33e418fa1ec908ed
thisminn <noblesse0308@naver.com>
2025-09-28 20:37:03 +0900

    feat:Header 좌측 디자인

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -2,8 +2,9 @@ import Reorder from '../assets/reorder.svg?react'
 export const Header = () => {
   return (
     <header className="w-full h-16 bg-[#FFFFFFE5] flex items-center">
-      <div>
+      <div className="flex justify-center items-center font-normal px-4 pt-4 pb-3text-[20px] leading-[28px] gap-2">
         <Reorder />
+        <p className="font-smooch">GITLOG</p>
       </div>
     </header>
   )
